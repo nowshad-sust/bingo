d447230e6f82b0db817a37f684c0eb41c6ef1cc2
nowshad-sust <iamnowshad@gmail.com>
2016-09-24 02:20:35 +0600

    first commit, making board from client side

---
+++ imports/ui/body.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import './body.html';
+import './game.js';
+
+Template.body.helpers({
+
+});
+
+Template.body.events({
+  
+});
---
+++ imports/ui/game.js
@@ -0,0 +1,45 @@
+import { Template } from 'meteor/templating';
+import './game.html';
+import './game.css';
+
+Template.game.helpers({
+	ifCheck: (index)=> {
+		if(index==5 || index==10 || index==15 || index==20){
+        return '</tr><tr>';
+    }else if(index==0){
+      return '<tr>';
+    }else if(index==24){
+      return '</tr>';
+    }else {
+      return null;
+    }
+	},
+  numbers: ()=>{
+    var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+    return shuffle(numbers);
+
+  }
+});
+
+Template.game.helpers({
+
+
+});
+
+Template.game.events({
+
+});
+function shuffle (array) {
+  var i = 0
+    , j = 0
+    , temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array;
+}
